c7647ff379df9b55effacd16395c9a0d7f816259
binay798 <binayshrestha798@gmail.com>
2025-10-31 17:35:51 +1100

    add: supported new templates

--- constants/templates.constants.ts
+++ constants/templates.constants.ts
@@ -10,4 +10,14 @@ export const TEMPLATE_LIST = [
     name: URL + "/vite-react-ts.json",
   },
   { id: "e1b4d4f7-8e93-420d-bc2d-05af7cdff01d", name: URL + "/vue-ts.json" },
+  { id: "f47ac10b-58cc-4372-a567-0e02b2c3d479", name: URL + "/next-js.json" },
+  {
+    id: "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+    name: URL + "/angular-js.json",
+  },
+  {
+    id: "3d6f4e2a-1b8c-4f5e-9a7d-8c2b1e4f6a3d",
+    name: URL + "/express-js.json",
+  },
+  { id: "a1b2c3d4-e5f6-4789-a012-3b4c5d6e7f8a", name: URL + "/hono-js.json" },
 ];
